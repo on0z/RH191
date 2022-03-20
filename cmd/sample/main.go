07f02535aa5e3bbe3f25eb58cf203f1aec2be3bd
on0z <onoz.dev@gmail.com>
2022-03-20 20:54:20 +0900

    ADRSIR用コードを出すサンプルを追加

---
+++ cmd/sample/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	librh191 "github.com/on0z/RH191"
+	"github.com/on0z/RH191/types"
+)
+
+func main() {
+	r := librh191.NewRH191(types.On, types.MODE_COOL, 26)
+	hex := r.GetHex()
+	fmt.Println(hex)
+	bin := r.GetBinary()
+	fmt.Println(bin)
+
+	adrsir := genADRSIR(bin)
+	fmt.Println(adrsir)
+}
+
+/// HexからADRSIRコードを生成する
+/// - Parameter hex: 送信するHex
+/// - Returns: ADRSIRコード
+func genADRSIR(binary string) string {
+	/// 始端
+	adrsir := "83004300"
+
+	/// 1と0をADRSIR表記にする
+	for _, bit := range binary {
+		if bit == '1' {
+			adrsir += "11003200"
+		} else if bit == '0' {
+			adrsir += "11001100"
+		}
+	}
+
+	/// 終端
+	adrsir += "1300EE01"
+
+	/// 2回繰り返して返却する
+	return adrsir + adrsir
+}
