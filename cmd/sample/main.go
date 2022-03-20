43e0b7f2fff30f471c81789911ff3ae5c67867a4
on0z <onoz.dev@gmail.com>
2022-03-20 21:59:29 +0900

    サンプルを動くものに更新

--- cmd/sample/main.go
+++ cmd/sample/main.go
@@ -1,27 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	libadrsir "github.com/on0z/libadrsir-go"
+	"periph.io/x/conn/v3/i2c"
+	"periph.io/x/conn/v3/i2c/i2creg"
+	host "periph.io/x/host/v3"
 
 	librh191 "github.com/on0z/RH191"
 	"github.com/on0z/RH191/types"
 )
 
 func main() {
-	r := librh191.NewRH191(types.On, types.ModeCool, 26)
-	hex := r.GetHex()
-	fmt.Println(hex)
-	bin := r.GetBinary()
-	fmt.Println(bin)
 
-	adrsir := genADRSIR(bin)
-	fmt.Println(adrsir)
+	// setup periph.io host
+	_, err := host.Init()
+	if err != nil {
+		log.Fatalf("failed to initialize periph: %v", err)
+	}
+
+	// Use i2creg I²C bus registry to find the first available I²C bus.
+	b, err := i2creg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer b.Close()
+
+	// Dev is a valid conn.Conn.
+	d := &i2c.Dev{Addr: uint16(libadrsir.ADDR), Bus: b}
+
+	// Setup ADRSIR
+	adrsir := libadrsir.NewADRSIR(d)
+
+	// Configure RH191
+	r := librh191.NewRH191(types.On, types.ModeHeat, 21)
+	r.SetDirection(types.DirectionDown)
+	// Generate Binary
+	bin := r.GetBinary()
+
+	// Convert binary to ADRSIR command
+	adrsirCmd := genADRSIRCmd(bin)
+
+	// Send command
+	adrsir.Send(adrsirCmd)
 }
 
-/// HexからADRSIRコードを生成する
+/// binaryからADRSIRコードを生成する
 /// - Parameter hex: 送信するHex
 /// - Returns: ADRSIRコード
-func genADRSIR(binary string) string {
+func genADRSIRCmd(binary string) string {
 	/// 始端
 	adrsir := "83004300"
 
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/on0z/RH191
 go 1.18
 
 require (
+	github.com/on0z/libadrsir-go v0.0.0-20220317173030-73a2a1c75943
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.1
+	periph.io/x/conn/v3 v3.6.10
+	periph.io/x/host/v3 v3.7.2
 )
 
 require (
